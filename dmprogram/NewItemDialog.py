ff7b02524cc6dd926edb8ee2d5950427fa674906
lemonaide512 <philliplemons@utexas.edu>
2013-07-27 00:50:58 -0500

    Added NewItemDialog and NewPlayerDialog documentation

--- dmprogram/NewItemDialog.py
+++ dmprogram/NewItemDialog.py
@@ -8,56 +8,59 @@ empty_stats = {'Name':'', 'Description':''}
 
 class NewItemDialog(wx.Dialog):
 
-	def __init__(self, parent, title, stats=empty_stats, *args, **kwargs):
-		super(NewItemDialog, self).__init__(parent, *args, **kwargs)
+    def __init__(self, parent, title, stats=empty_stats, *args, **kwargs):
+        super(NewItemDialog, self).__init__(parent, *args, **kwargs)
 
-		self.output=stats
-		self.InitUI()
-		self.SetSize((250,400))
-		self.SetTitle(title)
+        assert('Name' in stats)
+        assert('Description' in stats)
 
-	def InitUI(self):
-		pnl = wx.Panel(self, style=wx.BORDER_SIMPLE)
-		vbox1 = wx.BoxSizer(wx.VERTICAL)
-		vbox2 = wx.BoxSizer(wx.VERTICAL)
+        self.output=stats
+        self.InitUI()
+        self.SetSize((250,400))
+        self.SetTitle(title)
 
-		grid_sizer = wx.FlexGridSizer(rows=2, cols=2, vgap=5, hgap=5)
+    def InitUI(self):
+        pnl = wx.Panel(self, style=wx.BORDER_SIMPLE)
+        vbox1 = wx.BoxSizer(wx.VERTICAL)
+        vbox2 = wx.BoxSizer(wx.VERTICAL)
 
-		st1 = wx.StaticText(pnl, label='Name')
-		self.tctrl1 = wx.TextCtrl(pnl, value=str(self.output['Name']))
+        grid_sizer = wx.FlexGridSizer(rows=2, cols=2, vgap=5, hgap=5)
 
-		st2 = wx.StaticText(pnl, label='Desc')
-		self.tctrl2 = wx.TextCtrl(pnl, size=(150, 200), style=wx.TE_MULTILINE,
-								  value = str(self.output['Description']))
+        st1 = wx.StaticText(pnl, label='Name')
+        self.tctrl1 = wx.TextCtrl(pnl, value=str(self.output['Name']))
 
-		hbox = wx.BoxSizer(wx.HORIZONTAL)
-		okbtn = wx.Button(pnl, label='Ok', size=(85,28))
-		clsbtn = wx.Button(pnl, label='Close', size=(85,28))
-		hbox.Add(okbtn, flag=wx.RIGHT|wx.EXPAND, border=5)
-		hbox.Add(clsbtn, flag=wx.EXPAND)
+        st2 = wx.StaticText(pnl, label='Desc')
+        self.tctrl2 = wx.TextCtrl(pnl, size=(150, 200), style=wx.TE_MULTILINE,
+                                  value = str(self.output['Description']))
 
-		grid_sizer.AddMany( [(st1),(self.tctrl1, 1, wx.EXPAND),(st2),
-			                 (self.tctrl2, 1, wx.EXPAND)] )
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        okbtn = wx.Button(pnl, label='Ok', size=(85,28))
+        clsbtn = wx.Button(pnl, label='Close', size=(85,28))
+        hbox.Add(okbtn, flag=wx.RIGHT|wx.EXPAND, border=5)
+        hbox.Add(clsbtn, flag=wx.EXPAND)
 
-		grid_sizer.AddGrowableRow(1,1)
-		grid_sizer.AddGrowableCol(1,1)
+        grid_sizer.AddMany( [(st1),(self.tctrl1, 1, wx.EXPAND),(st2),
+                             (self.tctrl2, 1, wx.EXPAND)] )
 
-		vbox2.Add(grid_sizer, flag=wx.ALIGN_CENTER)
-		vbox2.Add(hbox, flag=wx.ALIGN_CENTER|wx.TOP|wx.LEFT|wx.RIGHT, border=5)
-		pnl.SetSizerAndFit(vbox2)
+        grid_sizer.AddGrowableRow(1,1)
+        grid_sizer.AddGrowableCol(1,1)
 
-		vbox1.Add(pnl,proportion=1,flag=wx.EXPAND|wx.ALL,border=10)
-		self.SetSizerAndFit(vbox1)
+        vbox2.Add(grid_sizer, flag=wx.ALIGN_CENTER)
+        vbox2.Add(hbox, flag=wx.ALIGN_CENTER|wx.TOP|wx.LEFT|wx.RIGHT, border=5)
+        pnl.SetSizerAndFit(vbox2)
 
-		self.Bind(wx.EVT_BUTTON, self.OnClose, clsbtn)
-		self.Bind(wx.EVT_BUTTON, self.OnOk, okbtn)
+        vbox1.Add(pnl,proportion=1,flag=wx.EXPAND|wx.ALL,border=10)
+        self.SetSizerAndFit(vbox1)
 
-		self.tctrl1.SetFocus()
+        self.Bind(wx.EVT_BUTTON, self.OnClose, clsbtn)
+        self.Bind(wx.EVT_BUTTON, self.OnOk, okbtn)
 
-	def OnClose(self, e):
-		self.Destroy()
+        self.tctrl1.SetFocus()
 
-	def OnOk(self, e):
-		self.output={'Name':self.tctrl1.GetValue(),
-				     'Description':self.tctrl2.GetValue()}
-		self.EndModal(wx.ID_OK)
+    def OnClose(self, e):
+        self.Destroy()
+
+    def OnOk(self, e):
+        self.output={'Name':self.tctrl1.GetValue(),
+                     'Description':self.tctrl2.GetValue()}
+        self.EndModal(wx.ID_OK)
